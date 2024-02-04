d89009b59d9032a4685acebb3f3a8e01d15c63ce
Boris Yarmulnik <boris.yarmoulnik@gmail.com>
2024-02-04 18:08:37 +0300

    Update random number

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,7 +1,4 @@
 const numberMin = 1;
 const numberMax = 100;
 
-// eslint-disable-next-line max-len
-const getRandomNumber = (min = numberMin, max = numberMax) => Math.floor(Math.random() * (max - min) + min);
-
-export default getRandomNumber;
+export default (min = numberMin, max = numberMax) => Math.floor(Math.random() * (max - min) + min);
